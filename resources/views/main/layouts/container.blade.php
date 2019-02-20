4ef406de3102f1c76da41ba3384b9b1a0b5989f0
Zoheir <zoheirkhonyagar@gmail.com>
2019-02-20 17:20:51 +0330

    finish master page

---
+++ resources/views/main/layouts/container.blade.php
@@ -0,0 +1,9 @@
+<div class="btContentWrap btClear">
+    <div class="btContentHolder">
+        <div class="btContent">
+            <div class="bt_rc_container">
+                @yield('content')
+            </div>
+        </div>
+    </div>
+</div>
--- resources/views/main/layouts/footer.blade.php
+++ resources/views/main/layouts/footer.blade.php
@@ -1,27 +1,24 @@
-        <!-- footer start -->
-        <div class="bt_rc_container">
-            <section id="bt_section5c4580bf7f197" class="boldSection topSmallSpaced bottomSmallSpaced btDarkSkin gutter inherit" style="background-color:#94774d;">
-                <div class="port">
-                    <div class="boldCell">
-                        <div class="boldCellInner">
-                            <div class="boldRow ">
-                                <div class="boldRowInner btTableRow">
-                                    <div class="rowItem col-md-12 col-ms-12  btTextLeft btMiddleVertical">
-                                        <div class="rowItemContent">
-                                            <div class="btText">
-                                                <p>تمامی حقوق متعلق به <a href="#" target="_blank" rel="nofollow noopener">گروه تولید محتوا ایما </a> میباشد</p>
-                                            </div>
-                                        </div>
+<!-- footer start -->
+<div class="bt_rc_container">
+    <section id="bt_section5c4580bf7f197" class="boldSection topSmallSpaced bottomSmallSpaced btDarkSkin gutter inherit" style="background-color:#94774d;">
+        <div class="port">
+            <div class="boldCell">
+                <div class="boldCellInner">
+                    <div class="boldRow ">
+                        <div class="boldRowInner btTableRow">
+                            <div class="rowItem col-md-12 col-ms-12  btTextLeft btMiddleVertical">
+                                <div class="rowItemContent">
+                                    <div class="btText">
+                                        <p>تمامی حقوق متعلق به <a href="#" target="_blank" rel="nofollow noopener">گروه تولید محتوا ایما </a> میباشد</p>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </section>
+            </div>
         </div>
-        <div class="btFooterWrap"></div>
-    </div>
-</body>
+    </section>
+</div>
+<div class="btFooterWrap"></div>
 
-</html>
